37bd6cf61fd0b4522652d7db7544bed16cfb632c
ThePromidius <thepromidiusyt@gmail.com>
2024-02-22 18:43:29 +0100

    changed output extensions json filename

--- updater/file_utils.py
+++ updater/file_utils.py
@@ -2,7 +2,7 @@ import json
 
 
 def load_sources_data():
-    sources_file_path = "sources.json"  # Replace with the actual path to your centralized sources.json
+    sources_file_path = "extensions.json"  # Replace with the actual path to your centralized extensions.json
     try:
         with open(sources_file_path, 'r') as sources_file:
             return json.load(sources_file)
@@ -11,7 +11,7 @@ def load_sources_data():
 
 
 def save_sources_data(data):
-    sources_file_path = "sources.json"  # Replace with the actual path to your centralized sources.json
+    sources_file_path = "extensions.json"  # Replace with the actual path to your centralized extensions.json
     with open(sources_file_path, 'w') as sources_file:
         json.dump(data, sources_file, indent=2)
 
